181f9ef84662c22c635b8a9dc6548f86c238d7f7
杨宇 <yangyu@jiancaiyi.cn>
2018-07-27 11:32:57 +0800

    压缩html

--- server/routers/server-render.js
+++ server/routers/server-render.js
@@ -1,14 +1,22 @@
 const ejs = require('ejs')
+const minifier = require('html-minifier').minify
 
 module.exports = async (ctx, renderer, template) => {
   ctx.headers['Content-Type'] = 'text/html'
   let context = {url: ctx.path}
   try {
     const appString = await renderer.renderToString(context)
-    const html = ejs.render(template, {
+    let html = ejs.render(template, {
       appString: appString,
       styles: context.renderStyles(),
       scripts: context.renderScripts()
+    })
+    // 压缩Html
+    html = minifier(html, {
+      removeComments: true,
+      collapseWhitespace: true,
+      minifyJS: true,
+      minifyCss: true
     })
     ctx.body = html
   } catch (err) {
